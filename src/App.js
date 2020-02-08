b68c4bec7c83d5cad8148f8b414c6489daeae526
thefyrewire <michael-hay369@hotmail.co.uk>
2020-02-08 20:30:14 +0000

    proxy api test

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 
 const App = () => {
+  const handleClick = async () => {
+    const data = await fetch('/api/test').then(d => d.json());
+    console.log(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <Button variant="contained" color="primary" href={`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${encodeURIComponent('http://localhost:3001/auth/callback')}&response_type=code&force_verify=true`}>Login</Button> */}
-        <Button variant="contained" color="primary" href={'http://localhost:3001/auth'}>Login</Button>
+        <Button variant="contained" color="primary" href={'http://localhost:5000/auth'}>Login</Button>
+        <Button variant="contained" color="primary" onClick={handleClick}>Get!</Button>
       </header>
     </div>
   );
